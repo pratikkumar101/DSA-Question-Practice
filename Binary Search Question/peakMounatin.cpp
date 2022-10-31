b15c5acc468faf3bba52536834dd8c3057c51ce9
pratikkumar008 <2002566,cse,cec@cgc.edu.in>
2022-10-31 23:39:07 +0530

    Program to find the peak index in an array

---
+++ Binary Search Question/peakMounatin.cpp
@@ -0,0 +1,31 @@
+#include "iostream"
+using namespace std;
+
+int peakMountain(int arr[], int n){
+    int s = 0;
+    int e = n-1;
+    int mid = s + (e - s) / 2;
+    while(s<e){
+        if(arr[mid] < arr[mid + 1]){
+            s = mid + 1;
+        }
+        else{
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+
+int main(){
+    int arr[100];
+    int n;
+    cin>>n;
+    for (int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    cout<<peakMountain(arr, n);
+
+    return 0;
+}
