e245bc67a0a2ad9ae228d47f0ac402ffe028e936
pratikkumar008 <2002566,cse,cec@cgc.edu.in>
2022-10-31 23:51:11 +0530

    Program to find the pivot in an array

---
+++ Binary Search Question/pivotInArray.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+using namespace std;
+
+int pivot(int arr[], int n){
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+    while(s < e){
+        if(arr[mid] >= arr[0]){
+            s = mid + 1;
+        }
+        else{
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+
+int main(){
+    int arr[100];
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> n;
+    }
+    cout<<pivot(arr, n);
+    return 0;
+}
